88dec55ae1157112a00244c2371130f444fcfe0b
rico_wu <jasonwu@outlook.com>
2021-05-29 21:10:04 +0800

    1

--- spdier/buy_jd.py
+++ spdier/buy_jd.py
@@ -1,64 +1,75 @@
 import time
 from playwright.sync_api import sync_playwright
 from selenium import webdriver
+from logzero import logger
+from multiprocessing import Process
+import requests
 
 
 def get_cookie(browser):
-    browser.get("https://passport.jd.com/new/login.aspx")
+    browser.get("https://passport.jd.com/uc/login?ltype=logout")
     while True:
-        if browser.current_url == "https://www.jd.com/":
-            break
-    return browser.get_cookies()
+        if "www.jd.com" in browser.current_url:
+            return browser.get_cookies()
 
 
-def buy_goods(browser, pid, cookie_dict: dict = None):
+def listen_item(browser, pid, cookies):
     browser.get("https://www.jd.com/")
-    if cookie_dict is None:
-        for i in get_cookie(browser):
-            browser.add_cookie(cookie_dict=i)
+    if isinstance(cookies, dict):
+        browser.add_cookie(cookies)
+    if isinstance(cookies, list):
+        for i in cookies:
+            browser.add_cookie(i)
     browser.get(f"https://item.jd.com/{pid}.html")
     while True:
-        selector = browser.find_element_by_link_text("加入购物车")
-        if selector:
-            selector.click()
+        try:
+            selector = browser.find_element_by_link_text("加入购物车")
             break
+        except:
+            browser.refresh()
+    selector.click()
+    logger.info(f"商品{pid}加入购物车")
+    browser.close()
+    return
+
+
+def listen_cart(browser, pid, cookies):
+    browser.get("https://www.jd.com/")
+    if isinstance(cookies, dict):
+        browser.add_cookie(cookies)
+    if isinstance(cookies, list):
+        for i in cookies:
+            browser.add_cookie(i)
     browser.get("https://cart.jd.com/cart_index/")
     while True:
-        selector = browser.find_element_by_link_text("去结算")
-        if selector:
-            selector.click()
+        try:
+            selector = browser.find_element_by_link_text("去结算")
             break
+        except:
+            browser.refresh()
+    selector.click()
+    logger.info(f"商品{pid}开始结算")
     while True:
-        selector = browser.find_element_by_id("order-submit")
-        if selector:
-            selector.click()
+        try:
+            selector = browser.find_element_by_id("order-submit")
             break
+        except:
+            browser.refresh()
+    selector.click()
+    logger.info(f"商品{pid}可以打开手机付款")
+    browser.close()
+    return
 
 
 if __name__ == "__main__":
-    browser = webdriver.Chrome()
-    buy_goods(browser, 5028827)
-
-# def buy(pid):
-#     with sync_playwright() as p:
-#         browser = p.chromium.launch(headless=False)
-#         context = browser.new_context()
-#         page = context.new_page()
-#         page.goto("https://passport.jd.com/new/login.aspx")
-#         page.wait_for_url("https://www.jd.com/",timeout=500000)
-#         try:
-#             page.goto(f"https://item.jd.com/{pid}.html",timeout=1200)
-#         except Exception as e:
-#             pass
-#         while True:
-#             try:
-#                 page.click(f'xpath=//a[@href="//cart.jd.com/gate.action?pid={pid}&pcount=1&ptype=1"]')
-#                 break
-#             except Exception as e:
-#                 continue
-#         page.goto("https://cart.jd.com/cart_index/")
-#         page.click('text=去结算')
-#         page.click('xpath=//div[@class="inner"]/button[@id="order-submit"]')
-#         time.sleep(200)
-# # https://plogin.m.jd.com/login/login?appid=300&returnurl=https%3A%2F%2Fwq.jd.com%2Fpassport%2FLoginRedirect%3Fstate%3D1101414989722%26returnurl%3Dhttps%253A%252F%252Fhome.m.jd.com%252FmyJd%252Fnewhome.action%253Fsceneval%253D2%2526ufc%253D%2526&source=wq_passport
-# # https://item.jd.com/5028827.html
+    browser_0 = webdriver.Chrome()
+    cookies = get_cookie(browser_0)
+    browser_0.close()
+    opts = webdriver.ChromeOptions()
+    # opts.set_headless()
+    # browser_1 = webdriver.Chrome(chrome_options=opts)
+    browser_2 = webdriver.Chrome(chrome_options=opts)
+    # listen_item =Process(target=listen_item,args=(browser_1,100021367452,cookies))
+    listen_cart = Process(target=listen_cart, args=(browser_2, 100021367452, cookies))
+    # listen_item.start()
+    listen_cart.start()
